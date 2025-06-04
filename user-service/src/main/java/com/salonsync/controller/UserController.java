ee068c17b4152abde327c849fdd385ac1e9d6af1
ima-69 <imansha.idr@gmail.com>
2025-06-05 00:12:03 +0530

    Update usercontroller

---
+++ user-service/src/main/java/com/salonsync/controller/UserController.java
@@ -0,0 +1,53 @@
+package com.salonsync.controller;
+
+import com.salonsync.exception.UserException;
+import com.salonsync.model.User;
+import com.salonsync.repository.UserRepository;
+import com.salonsync.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/api/users")
+    public ResponseEntity<User> createUser(@RequestBody @Valid User user) {
+        User createdUser = userService.createUser(user);
+        return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/api/users/all")
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userService.getAllUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+
+    @GetMapping("/api/users/{userId}")
+    public ResponseEntity<User> getUserById(@PathVariable("userId") Long id) throws Exception{
+        User user = userService.getUserById(id);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @PutMapping("api/user/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable("id") Long id, @RequestBody @Valid User userDetails) throws Exception {
+        User updatedUser = userService.updateUser(id, userDetails);
+        return new ResponseEntity<>(updatedUser, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/api/users/{userId}")
+    public ResponseEntity<String> deleteUser(@PathVariable("userId") Long id) throws Exception {
+        userService.deleteUser(id);
+        return new ResponseEntity<>("User deleted", HttpStatus.ACCEPTED);
+    }
+
+}
