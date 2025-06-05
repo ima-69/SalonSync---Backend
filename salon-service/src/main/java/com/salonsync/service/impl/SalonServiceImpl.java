710c89f21fc6b6d8f27c8fbc159b8da6ed32331a
ima-69 <imansha.idr@gmail.com>
2025-06-05 10:54:47 +0530

    created create salon salon service impl

--- salon-service/src/main/java/com/salonsync/service/impl/SalonServiceImpl.java
+++ salon-service/src/main/java/com/salonsync/service/impl/SalonServiceImpl.java
@@ -3,17 +3,33 @@ package com.salonsync.service.impl;
 import com.salonsync.model.Salon;
 import com.salonsync.payload.dto.SalonDTO;
 import com.salonsync.payload.dto.UserDTO;
+import com.salonsync.repository.SalonRepository;
 import com.salonsync.service.SalonService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class SalonServiceImpl implements SalonService {
 
+    private final SalonRepository salonRepository;
+
     @Override
-    public Salon createSalon(SalonDTO salon, UserDTO user) {
-        return null;
+    public Salon createSalon(SalonDTO req, UserDTO user) {
+        Salon salon = new Salon();
+        salon.setName(req.getName());
+        salon.setAddress(req.getAddress());
+        salon.setCity(req.getCity());
+        salon.setEmail(req.getEmail());
+        salon.setPhoneNumber(req.getPhoneNumber());
+        salon.setOwnerId(user.getId());
+        salon.setImages(req.getImages());
+        salon.setOpenTime(req.getOpenTime());
+        salon.setCloseTime(req.getCloseTime());
+
+        return salonRepository.save(salon);
     }
 
     @Override
