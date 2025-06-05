6f6635c2ef2f79fd0ab474cb320abee9e8547154
ima-69 <imansha.idr@gmail.com>
2025-06-05 18:06:18 +0530

    Create CategoryRepository.java

---
+++ category-service/src/main/java/com/salonsync/repository/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.salonsync.repository;
+
+import com.salonsync.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Set;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    Set<Category> findBySalonId(Long salonId);
+}
