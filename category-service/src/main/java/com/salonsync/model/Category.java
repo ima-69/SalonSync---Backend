d899d2d151f335150786aef96f0683a2ebf36ed3
ima-69 <imansha.idr@gmail.com>
2025-06-05 18:05:50 +0530

    Update Category.java

--- category-service/src/main/java/com/salonsync/model/Category.java
+++ category-service/src/main/java/com/salonsync/model/Category.java
@@ -1,18 +1,25 @@
 package com.salonsync.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Category {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @Column(unique = false)
+    @Column(nullable = false)
     private String name;
 
     private String image;
 
+    @Column(nullable = false)
     private Long salonId;
 }
