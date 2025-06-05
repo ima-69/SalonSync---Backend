fd6edc9dbc42f0eb0cb0070dcbcd956c45a05df4
ima-69 <imansha.idr@gmail.com>
2025-06-06 02:19:47 +0530

    Create ServiceOfferingServiceImpl.java

---
+++ service-offering/src/main/java/com/salonsync/service/impl/ServiceOfferingServiceImpl.java
@@ -0,0 +1,39 @@
+package com.salonsync.service.impl;
+
+import com.salonsync.dto.CategoryDTO;
+import com.salonsync.dto.SalonDTO;
+import com.salonsync.dto.ServiceDTO;
+import com.salonsync.model.ServiceOffering;
+import com.salonsync.repository.ServiceOfferingRepository;
+import com.salonsync.service.ServiceOfferingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+@RequiredArgsConstructor
+public class ServiceOfferingServiceImpl implements ServiceOfferingService {
+
+    private final ServiceOfferingRepository serviceOfferingRepository;
+
+    @Override
+    public ServiceOffering createService(SalonDTO salonDTO, ServiceDTO serviceDTO, CategoryDTO categoryDTO) {
+        return null;
+    }
+
+    @Override
+    public ServiceOffering updateService(Long salonId, ServiceOffering service) {
+        return null;
+    }
+
+    @Override
+    public Set<ServiceOffering> getAllServicesBySalonId(Long salonId, Long categoryId) {
+        return Set.of();
+    }
+
+    @Override
+    public Set<ServiceOffering> getServicesByIds(Set<Long> ids) {
+        return Set.of();
+    }
+}
