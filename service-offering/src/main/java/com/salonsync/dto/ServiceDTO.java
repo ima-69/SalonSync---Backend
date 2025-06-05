634d9226c56882aac584ac232eda97167a565a07
ima-69 <imansha.idr@gmail.com>
2025-06-06 02:19:54 +0530

    Create ServiceDTO.java

---
+++ service-offering/src/main/java/com/salonsync/dto/ServiceDTO.java
@@ -0,0 +1,24 @@
+package com.salonsync.dto;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ServiceDTO {
+
+    private Long id;
+    private String name;
+    private String description;
+    private int price;
+    private int duration;
+    private Long salonId;
+    private Long categoryId;
+    private String image;
+}
