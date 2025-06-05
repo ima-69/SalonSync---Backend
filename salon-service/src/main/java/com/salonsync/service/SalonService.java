31757eee0475e4851b51a7e0ce2ee5b552e6bb6b
ima-69 <imansha.idr@gmail.com>
2025-06-05 10:27:15 +0530

    Create SalonService.java

---
+++ salon-service/src/main/java/com/salonsync/service/SalonService.java
@@ -0,0 +1,23 @@
+package com.salonsync.service;
+
+import com.salonsync.model.Salon;
+import com.salonsync.payload.dto.SalonDTO;
+import com.salonsync.payload.dto.UserDTO;
+
+import java.util.List;
+
+public interface SalonService {
+
+    Salon createSalon(SalonDTO salon, UserDTO user);
+
+    Salon updateSalon(SalonDTO salon, UserDTO user, Long salonId);
+
+    List<Salon> getAllSalons();
+
+    Salon getSalonById(Long salonId);
+
+    Salon getSalonByOwnerId(Long ownerId);
+
+    List<Salon> getSalonByCity(String city);
+
+}
