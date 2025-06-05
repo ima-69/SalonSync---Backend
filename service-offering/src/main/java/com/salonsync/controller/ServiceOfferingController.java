5a887f189da9318f5458fc35452ab41893f7c405
ima-69 <imansha.idr@gmail.com>
2025-06-06 03:09:50 +0530

    Create ServiceOfferingController.java

---
+++ service-offering/src/main/java/com/salonsync/controller/ServiceOfferingController.java
@@ -0,0 +1,26 @@
+package com.salonsync.controller;
+
+import com.salonsync.model.ServiceOffering;
+import com.salonsync.service.ServiceOfferingService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
+
+@RestController
+@RequestMapping("/api/service-offering")
+public class ServiceOfferingController {
+
+    private ServiceOfferingService serviceOfferingService;
+
+    @GetMapping("/salon/{salonId}")
+    public ResponseEntity<Set<ServiceOffering>> getServicesBySalonId(
+            @PathVariable Long salonId,
+            @RequestParam(required = false) Long categoryId
+    ) {
+        Set<ServiceOffering> services = serviceOfferingService.getAllServicesBySalonId(salonId, categoryId);
+        return ResponseEntity.ok(services);
+
+    }
+
+}
