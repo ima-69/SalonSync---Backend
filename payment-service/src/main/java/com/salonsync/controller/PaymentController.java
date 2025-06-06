4f2f178cf0d5e70dea95c5dc8fb134aed5ba869e
ima-69 <imansha.idr@gmail.com>
2025-06-07 01:08:41 +0530

    createPaymentLink controller method implemented

--- payment-service/src/main/java/com/salonsync/controller/PaymentController.java
+++ payment-service/src/main/java/com/salonsync/controller/PaymentController.java
@@ -1,9 +1,15 @@
 package com.salonsync.controller;
 
+import com.razorpay.RazorpayException;
+import com.salonsync.domain.PaymentMethod;
+import com.salonsync.payload.dto.BookingDTO;
+import com.salonsync.payload.dto.UserDTO;
+import com.salonsync.payload.response.PaymentLinkResponse;
 import com.salonsync.service.PaymentService;
+import com.stripe.exception.StripeException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/payments")
@@ -12,5 +18,19 @@ public class PaymentController {
 
     private final PaymentService paymentService;
 
-    
+    @PostMapping("/create")
+    public ResponseEntity<PaymentLinkResponse> createPaymentLink(
+            @RequestBody BookingDTO booking,
+            @RequestParam PaymentMethod paymentMethod
+    ) throws StripeException, RazorpayException {
+        UserDTO user = new UserDTO();
+        user.setFirstName("Imansha");
+        user.setLastName("Dilshan");
+        user.setEmail("imansha@gmail.com");
+        user.setId(1L);
+
+        PaymentLinkResponse res = paymentService.createOrder(user, booking, paymentMethod);
+
+        return ResponseEntity.ok(res);
+    }
 }
