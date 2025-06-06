e2e4e444b2b2d72bc60aa136fb42adf7fef82100
ima-69 <imansha.idr@gmail.com>
2025-06-06 16:05:47 +0530

    updateBookingStatus controller method created

--- booking-service/src/main/java/com/salonsync/service/impl/BookingServiceImpl.java
+++ booking-service/src/main/java/com/salonsync/service/impl/BookingServiceImpl.java
@@ -110,8 +110,12 @@ public class BookingServiceImpl implements BookingService {
     }
 
     @Override
-    public Booking updateBooking(Long bookingId, BookingStatus status) {
-        return null;
+    public Booking updateBooking(Long bookingId, BookingStatus status) throws Exception {
+
+        Booking booking = getBookingById(bookingId);
+        booking.setStatus(status);
+
+        return bookingRepository.save(booking);
     }
 
     @Override
