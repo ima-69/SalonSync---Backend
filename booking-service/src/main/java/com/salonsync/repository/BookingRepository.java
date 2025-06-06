a3752202d3e2994cb9f508a99cd6177ef85bd4e6
ima-69 <imansha.idr@gmail.com>
2025-06-06 13:24:17 +0530

    Create BookingRepository.java

---
+++ booking-service/src/main/java/com/salonsync/repository/BookingRepository.java
@@ -0,0 +1,13 @@
+package com.salonsync.repository;
+
+import com.salonsync.modal.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BookingRepository extends JpaRepository<Booking, Long> {
+
+    List<Booking> findByCustomerId(Long customerId);
+    List<Booking> findBySalonId(Long salonId);
+    
+}
