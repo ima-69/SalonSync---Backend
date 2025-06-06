e9ed9906c548dfcab0c159b8878021bd805a66bb
ima-69 <imansha.idr@gmail.com>
2025-06-06 22:40:57 +0530

    Create BookingRequest.java

---
+++ payment-service/src/main/java/com/salonsync/payload/dto/BookingRequest.java
@@ -0,0 +1,17 @@
+package com.salonsync.payload.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class BookingRequest {
+     private LocalDateTime startTime;
+     private LocalDateTime endTime;
+     private Set<Long> serviceIds;
+}
