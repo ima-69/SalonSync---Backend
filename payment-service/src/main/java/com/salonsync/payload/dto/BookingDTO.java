d0756f4b4e75be6e5b4d15681c8e80f6ac206db4
ima-69 <imansha.idr@gmail.com>
2025-06-06 22:40:52 +0530

    Create BookingDTO.java

---
+++ payment-service/src/main/java/com/salonsync/payload/dto/BookingDTO.java
@@ -0,0 +1,23 @@
+package com.salonsync.payload.dto;
+
+import com.salonsync.domain.BookingStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class BookingDTO {
+    private Long id;
+    private Long salonId;
+    private Long customerId;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private Set<Long> serviceIds;
+    private BookingStatus status = BookingStatus.PENDING;
+
+}
