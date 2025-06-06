8dae521b004de6be758cb0ca110dc79e94d17a72
ima-69 <imansha.idr@gmail.com>
2025-06-06 15:48:20 +0530

    getBookingsByCustomer controller method created

--- booking-service/src/main/java/com/salonsync/service/impl/BookingServiceImpl.java
+++ booking-service/src/main/java/com/salonsync/service/impl/BookingServiceImpl.java
@@ -92,7 +92,7 @@ public class BookingServiceImpl implements BookingService {
 
     @Override
     public List<Booking> getBookingsByCustomer(Long customerId) {
-        return List.of();
+        return bookingRepository.findByCustomerId(customerId);
     }
 
     @Override
