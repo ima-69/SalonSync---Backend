4c597d7675db7fb9b4c2e05a8801e2181300b46e
ima-69 <imansha.idr@gmail.com>
2025-06-09 00:00:16 +0530

    Create SignupDTO.java

---
+++ user-service/src/main/java/com/salonsync/payload/dto/SignupDTO.java
@@ -0,0 +1,9 @@
+package com.salonsync.payload.dto;
+
+public class SignupDTO {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String username;
+}
