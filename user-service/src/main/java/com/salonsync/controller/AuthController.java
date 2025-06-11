5ef8c73ec1d20e61c88af1cbe858948eb992c355
ima-69 <imansha.idr@gmail.com>
2025-06-11 11:47:25 +0530

    login authcontroller method implemented

--- user-service/src/main/java/com/salonsync/controller/AuthController.java
+++ user-service/src/main/java/com/salonsync/controller/AuthController.java
@@ -1,10 +1,12 @@
 package com.salonsync.controller;
 
+import com.salonsync.payload.dto.LoginDTO;
 import com.salonsync.payload.dto.SignupDTO;
 import com.salonsync.payload.response.AuthResponse;
 import com.salonsync.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,13 +18,39 @@ public class AuthController {
 
     private final AuthService authService;
 
+    @PostMapping("/signup")
     public ResponseEntity<AuthResponse> signup(
             @RequestBody SignupDTO req
     ) throws Exception {
 
         AuthResponse res = authService.signUp(req);
-
         return ResponseEntity.ok(res);
     }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(
+            @RequestBody LoginDTO req
+    ) throws Exception {
+
+        AuthResponse res = authService.login(
+                req.getUsername(),
+                req.getPassword()
+        );
+        return ResponseEntity.ok(res);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(
+            @RequestBody LoginDTO req
+    ) throws Exception {
+
+        AuthResponse res = authService.login(
+                req.getUsername(),
+                req.getPassword()
+        );
+        return ResponseEntity.ok(res);
+    }
+
+
 
 }
