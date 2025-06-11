00708feb5484801ff575480dd92949fe639ce52d
ima-69 <imansha.idr@gmail.com>
2025-06-11 11:36:57 +0530

    signup authservice controller method implemented

--- user-service/src/main/java/com/salonsync/controller/AuthController.java
+++ user-service/src/main/java/com/salonsync/controller/AuthController.java
@@ -1,6 +1,11 @@
 package com.salonsync.controller;
 
+import com.salonsync.payload.dto.SignupDTO;
+import com.salonsync.payload.response.AuthResponse;
+import com.salonsync.service.AuthService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 public class AuthController {
 
-    
+    private final AuthService authService;
+
+    public ResponseEntity<AuthResponse> signup(
+            @RequestBody SignupDTO req
+    ) throws Exception {
+
+        AuthResponse res = authService.signUp(req);
+
+        return ResponseEntity.ok(res);
+    }
 
 }
