a248f9d9270d0b8083b3e74aff29fefacecfbc30
ima-69 <imansha.idr@gmail.com>
2025-06-11 11:33:42 +0530

    Create AuthController.java

---
+++ user-service/src/main/java/com/salonsync/controller/AuthController.java
@@ -0,0 +1,14 @@
+package com.salonsync.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth")
+public class AuthController {
+
+    
+
+}
