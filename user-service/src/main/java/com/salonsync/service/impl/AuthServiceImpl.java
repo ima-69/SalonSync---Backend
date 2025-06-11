255ec3c6d361892cbd41a6981d010c3c97397612
ima-69 <imansha.idr@gmail.com>
2025-06-11 11:21:49 +0530

    signUp authservice method implemented

--- user-service/src/main/java/com/salonsync/service/impl/AuthServiceImpl.java
+++ user-service/src/main/java/com/salonsync/service/impl/AuthServiceImpl.java
@@ -1,9 +1,23 @@
 package com.salonsync.service.impl;
 
+import com.salonsync.model.User;
+import com.salonsync.payload.dto.SignupDTO;
 import com.salonsync.payload.response.AuthResponse;
+import com.salonsync.payload.response.TokenResponse;
+import com.salonsync.repository.UserRepository;
 import com.salonsync.service.AuthService;
+import com.salonsync.service.KeycloakService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService {
+
+    private final UserRepository userRepository;
+    private final KeycloakService keycloakService;
 
     @Override
     public AuthResponse login(String username, String password) {
@@ -11,8 +25,36 @@ public class AuthServiceImpl implements AuthService {
     }
 
     @Override
-    public AuthResponse signUp(String username, String password) {
-        return null;
+    public AuthResponse signUp(SignupDTO req) throws Exception {
+
+        keycloakService.createUser(req);
+
+        User user = new User();
+        user.setUsername(req.getUsername());
+        user.setPassword(req.getPassword());
+        user.setEmail(req.getEmail());
+        user.setRole(req.getRole());
+        user.setFirstName(req.getFirstName());
+        user.setLastName(req.getLastName());
+        user.setCreatedAt(LocalDateTime.now());
+
+        user = userRepository.save(user);
+
+        TokenResponse tokenResponse = keycloakService.getAdminAccessToken(
+                req.getUsername(),
+                req.getPassword(),
+                "password",
+                null
+        );
+
+        AuthResponse authResponse = new AuthResponse();
+        authResponse.setRefreshToken(tokenResponse.getRefreshToken());
+        authResponse.setJwt(tokenResponse.getAccessToken());
+        authResponse.setRole(user.getRole());
+        authResponse.setMessage("User registered successfully");
+
+
+        return authResponse;
     }
 
     @Override
