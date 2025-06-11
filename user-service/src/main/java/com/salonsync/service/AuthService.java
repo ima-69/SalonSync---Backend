8c7d2aff2dda77bb6f7be6f8ba67b1d607fd2624
ima-69 <imansha.idr@gmail.com>
2025-06-11 11:21:05 +0530

    Update AuthService.java

--- user-service/src/main/java/com/salonsync/service/AuthService.java
+++ user-service/src/main/java/com/salonsync/service/AuthService.java
@@ -1,11 +1,12 @@
 package com.salonsync.service;
 
+import com.salonsync.payload.dto.SignupDTO;
 import com.salonsync.payload.response.AuthResponse;
 
 public interface AuthService {
 
     AuthResponse login(String username, String password);
-    AuthResponse signUp(String username, String password);
+    AuthResponse signUp(SignupDTO req) throws Exception;
     AuthResponse getAccessTokenFromRefreshToken(String refreshToken);
 
 }
