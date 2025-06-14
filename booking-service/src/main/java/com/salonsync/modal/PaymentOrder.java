90e1cd1701770f8be9b783e6328b0afd255fe8d5
ima-69 <imansha.idr@gmail.com>
2025-06-14 11:38:48 +0530

    Create PaymentOrder.java

---
+++ booking-service/src/main/java/com/salonsync/modal/PaymentOrder.java
@@ -0,0 +1,35 @@
+package com.salonsync.modal;
+
+import com.salonsync.domain.PaymentMethod;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class PaymentOrder {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long amount;
+
+    @Column(nullable = false)
+    private PaymentMethod paymentMethod;
+
+    private String paymentLinkId;
+
+    @Column(nullable = false)
+    private Long userId;
+
+    @Column(nullable = false)
+    private Long bookingId;
+
+    @Column(nullable = false)
+    private Long salonId;
+}
