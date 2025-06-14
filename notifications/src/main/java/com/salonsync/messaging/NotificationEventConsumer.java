0319dd4c7920961bcb6e2463e3fc1d394b256e54
ima-69 <imansha.idr@gmail.com>
2025-06-14 11:45:23 +0530

    Create NotificationEventConsumer.java

---
+++ notifications/src/main/java/com/salonsync/messaging/NotificationEventConsumer.java
@@ -0,0 +1,20 @@
+package com.salonsync.messaging;
+
+import com.salonsync.model.Notification;
+import com.salonsync.service.NotificationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class NotificationEventConsumer {
+
+    private final NotificationService notificationService;
+
+    @RabbitListener(queues = "notification-queue")
+    public void sentBookingUpdateEvent(Notification notification){
+        notificationService.createNotification(notification);
+    }
+
+}
