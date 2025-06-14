5a453cada9f23d5b9c10e37b82b7f76483247879
ima-69 <imansha.idr@gmail.com>
2025-06-14 11:18:34 +0530

    Create BookingEventProducer.java

---
+++ payment-service/src/main/java/com/salonsync/messaging/BookingEventProducer.java
@@ -0,0 +1,17 @@
+package com.salonsync.messaging;
+
+import com.salonsync.model.PaymentOrder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class BookingEventProducer {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public void sentBookingUpdateEvent(PaymentOrder paymentOrder){
+        rabbitTemplate.convertAndSend("booking-queue", paymentOrder);
+    }
+}
